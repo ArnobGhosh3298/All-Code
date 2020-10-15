e6a6153807897bdfab6bd59cc64562d7453e3be9
ArnobGhosh3298 <68845072+ArnobGhosh3298@users.noreply.github.com>
2020-10-15 15:40:16 +0600

    add comment

--- Uri/Uri1066.c
+++ Uri/Uri1066.c
@@ -1,31 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int a[5],i=0,even=0,odd=0,pos=0,neg=0;
-    while(i<5)
+    int a[5], i = 0, even = 0, odd = 0, pos = 0, neg = 0;
+    while (i < 5)
     {
-        scanf("%d",&a[i]);
-        if(a[i]%2==0)
-            {
+        scanf("%d", &a[i]);
+        if (a[i] % 2 == 0)
+        {
             even++;
-            }
-        if(a[i]%2!=0)
+        }
+        if (a[i] % 2 != 0)
         {
             odd++;
-            }
-        if(a[i]>0)
+        }
+        if (a[i] > 0)
         {
             pos++;
-            }
-        if(a[i]<0)
+        }
+        if (a[i] < 0)
         {
             neg++;
-            }
+        }
         i++;
     }
-    printf("%d valor(es) par(es)\n",even);
-    printf("%d valor(es) impar(es)\n",odd);
-    printf("%d valor(es) positivo(s)\n",pos);
-    printf("%d valor(es) negativo(s)\n",neg);
+    printf("%d valor(es) par(es)\n", even);
+    printf("%d valor(es) impar(es)\n", odd);
+    printf("%d valor(es) positivo(s)\n", pos);
+    printf("%d valor(es) negativo(s)\n", neg);
+    // this is nothing
     return 0;
 }
